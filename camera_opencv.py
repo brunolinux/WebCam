6b9cc8bfd93f9cfeacd2a50b850c84311d6ca79f
brunolinux <bruno.liu@qq.com>
2018-11-08 13:26:07 +0100

    Function: Face detection

--- camera_opencv.py
+++ camera_opencv.py
@@ -2,6 +2,9 @@ import cv2
 from base_camera import BaseCamera
 
 
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
 class Camera(BaseCamera):
     @staticmethod
     def frames():
@@ -12,6 +15,20 @@ class Camera(BaseCamera):
         while True:
             # read current frame
             _, img = camera.read()
+
+            # face detection
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30))
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
