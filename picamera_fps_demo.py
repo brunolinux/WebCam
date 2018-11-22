9faadc55156b859681bbf1ed7328215152f95704
brunoLinux <kang.liu.edu@gmail.com>
2018-11-22 14:07:34 +0100

    cv Bug fix_2

--- picamera_fps_demo.py
+++ picamera_fps_demo.py
@@ -35,7 +35,7 @@ for (i, f) in enumerate(stream):
 	# grab the frame from the stream and resize it to have a maximum
 	# width of 400 pixels
 	frame = f.array
-	frame = imutils.resize(frame, width=400)
+	#frame = imutils.resize(frame, width=400)
  
 	# check to see if the frame should be displayed to our screen
 	#if args["display"] > 0:
@@ -76,7 +76,7 @@ while fps._numFrames < args["num_frames"]:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	#frame = imutils.resize(frame, width=400)
  
 	# check to see if the frame should be displayed to our screen
 	#if args["display"] > 0:
